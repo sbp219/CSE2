fc01c0010e6335d5c7aed6ad644cd8a372ffb253
sbp219 <sbp219@lehigh.edu>
2015-08-30 21:27:22 +0000

    hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+//Saral Patel
+//Demo 0
+//Welcome Class Java Program
+//
+// first compile the program
+//      javac WelcomeClass.java
+// run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+ // add main method
+    public static void main(String[ ]   args) {
+   
+// print statement with box around it
+    System.out.println("    ----------- ");
+    System.out.println("    | WELCOME | ");
+    System.out.println("    ----------- ");
+    System.out.println("  ^  ^  ^  ^  ^  ^ ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-S--B--P--2--1--9->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v ");
+    System.out.println(" So far Lehigh has been an experience like none other. I am glad I chose this school.");
+    
+
+    //test/testtest
+    }
+    
+}
